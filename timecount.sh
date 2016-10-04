748fe9e0a3653610c48c6bf6c012b5bcdfc96297
Kevin_gpu2 <wuhaoxuan1993@163.com>
2016-10-04 11:13:34 +0800

    add timecount file

--- timecount.sh
+++ timecount.sh
@@ -1,1 +1,9 @@
-#!bin/sh
+#!bin/bash
+start_time=`date +%s`
+#sh examples/mnist/train_lenet.sh
+sleep 2
+end_time=`date +%s`
+echo
+echo
+echo '==============================================='
+echo "the total time cost is `expr $end_time - $start_time` s"
